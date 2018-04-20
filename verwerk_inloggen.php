ebc91972299bd45b2ea8f7824a3998b275f40020
HHK3 <joeltilon@live.nl>
2018-04-20 19:14:09 +0200

    V1.0 (Cleaned a little spot up)

---
+++ verwerk_inloggen.php
@@ -0,0 +1,49 @@
+<?php
+
+session_start();
+
+// Hoort de bezoeker hier uberhaupt wel te zijn?
+if (!isset($_POST['submit_log'])) {
+    header('Location: index.php');
+}
+
+//// Checken of de gebruiker alles heeft ingevuld
+//if (empty($_POST['username_log']) OR empty($_POST['password'])) {
+//    header("Location: index.php?login=empty");
+//    echo 'Je bent iets vergeten in te vullen <br>';
+//    echo 'Klik <a href="index.php">hier</a> om het nog eens te proberen.';
+//    exit();
+//}
+
+// Checken of de gebruiker bestaat (en of zijn wachtwoord klopt)
+require ('private/connection.php');
+$query = "SELECT userid, hash, active FROM gebruikers WHERE username = ? AND password = ?";
+$stmt = $mysqli->prepare($query) or die ('Error preparing');
+$stmt->bind_param('ss', $username, $password) or die ('Error binding params');
+$stmt->bind_result($userid, $hash, $active) or die ('Error binding results');
+$username = $_POST['username_log'];
+$password = $_POST['password'];
+$password = hash('sha512', $password) or die ('Error hashing.');
+$stmt->execute() or die ('Error executing');
+$fetch_succes = $stmt->fetch();
+
+if (!$fetch_succes) {
+    header('Location: index.php?login=error');
+    exit();
+
+} else if ($active == 0) {
+    header('Location: index.php?login=unverified');
+    exit();
+}
+
+setcookie('userid', $userid, time() + 3600 * 24 * 7);
+$_SESSION['userid'] = $userid;
+
+setcookie('hash', $hash, time() + 3600 * 24 * 7);
+$_SESSION['hash'] = $hash;
+
+setcookie('username', $username, time() + 3600 * 24 * 7);
+$_SESSION['username'] = $username;
+
+header('Location: url=wall.php');
+?>
